dd2155965172d50709844a6d390ff77a1f9af01b
Kim-Phantom <138415399+Kim-Phantom@users.noreply.github.com>
2023-07-26 17:35:56 +0900

    Add files via upload

---
+++ ch08_collection/note.java
@@ -0,0 +1,15 @@
+package ch08_collection;
+
+public class note {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		String person = new Person();
+		person.name = "John";
+		person.age = 30;
+		person.introduce();
+    
+	}
+
+}
